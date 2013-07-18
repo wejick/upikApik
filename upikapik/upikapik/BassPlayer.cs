4f1f47302d80fe5b530979e5a4febefc407db7b7
Gian Giovani <wejick@gmail.com>
2013-07-18 10:53:29 +0700

    Implement control methods

--- upikapik/upikapik/BassPlayer.cs
+++ upikapik/upikapik/BassPlayer.cs
@@ -23,8 +23,7 @@ namespace upikapik
         private System.Timers.Timer mainTime;
 
         /*
-         * < initialize everything to get bass set and ready >
-         * 
+         * < initialize everything to get bass set and ready >         * 
          * */
         public BassPlayer()
         {
@@ -63,7 +62,7 @@ namespace upikapik
          * */
         public string getFileName()
         {
-            return System.IO.Path.GetFileName(path);
+            return path2FileName(path);
         }
         /*
          * < Get the length from stream source in byte>
@@ -99,19 +98,77 @@ namespace upikapik
         }
 
         // control the stream
-        public void play(string path);
-        public void pause_resume();
-        public void stop();
-        public void seek(int pos);
+        /*
+         * < Play the stream >
+         * @param path of source file
+         * */
+        public void play(string path)
+        {
+            this.setPath(path);
+            if ((stream = Bass.BASS_StreamCreateFile(path, 0L, 0L, BASSFlag.BASS_DEFAULT)) != 0)
+            {
+                Bass.BASS_ChannelPlay(stream, false);
+                streamLen = Bass.BASS_ChannelGetLength(stream);
+            }
+            else
+                throw new System.InvalidOperationException("Can't open file to play");
+        }
+        /*
+         * < Pause or resume the stream according to current state of stream >
+         * 
+         * */
+        public void pause_resume()
+        {
+            if (stream != 0)
+            {
+                // if stream playing, pause it
+                if (Bass.BASS_ChannelIsActive(stream) == BASSActive.BASS_ACTIVE_PLAYING)
+                    Bass.BASS_ChannelPause(stream);
+                // if stream paused, play it
+                else if (Bass.BASS_ChannelIsActive(stream) == BASSActive.BASS_ACTIVE_PAUSED)
+                    Bass.BASS_ChannelPlay(stream, false);
+            }
+        }
+        /*
+         * < Stop the stream >
+         * */
+        public void stop()
+        {
+            Bass.BASS_ChannelStop(stream);
+        }
+        /*
+         * < Seek the stream to certain position >
+         * @param pos is desired position in seconds
+         * */
+        public void seek(int pos)
+        {
+            Bass.BASS_ChannelSetPosition(stream, pos);
+        }
 
         // bass spesific good routine
-        public void free();
+        /*
+         * < Free resource used by bass >
+         * */
+        public void free()
+        {
+            Bass.BASS_Free();
+        }
 
         // utility
-        private string path2FileName(string path);
-        private static void onMainTime(object source, ElapsedEventArgs e)
+        /*
+         * < Take filename from complete path >
+         * @param a string of path
+         * */
+        private string path2FileName(string path)
         {
-
+            return System.IO.Path.GetFileName(path);
+        }
+        /*
+         * < timer to maintain dirty job :p >
+         * */
+        private void onMainTime(object source, ElapsedEventArgs e)
+        {
+            streamPos = Bass.BASS_ChannelGetPosition(stream);
         }
     }
 }
