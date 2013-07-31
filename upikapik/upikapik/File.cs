805fc49303a3d194ad6c6386ab5b08559f4db4ea
Gian Giovani <wejick@gmail.com>
2013-07-31 14:55:44 +0700

    UPDATE

---
+++ upikapik/upikapik/File.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace upikapik
+{
+    class File
+    {
+        public File(string path)
+        {
+            this.path = path;
+            // create file if none
+            // set filename
+            // get bitrate
+            // get padding
+            // get samplerate
+            // get size
+            // calculate frame length
+        }
+        private string path { get; set; }
+        private string filename { get; set;}
+        private int frameLength;
+        private int bitrate;
+        private int samplerate;
+        private int size;
+        private bool padding;
+    }
+}
--- upikapik/upikapik/db.cs
+++ upikapik/upikapik/db.cs
@@ -11,26 +11,37 @@ namespace upikapik
 {
     class file_host_rel
     {
-        string fileId { get; set; }
-        string nodeIP { get; set; }
-        int block_avail { get; set; }
+        public string id_file { get; set; }
+        public string ip { get; set; }
+        public int block_avail { get; set; }
     }
     class file_list
     {
-        string fileId { get; set; }
-        string filename { get; set; }
-        int bitrate { get; set; }
-        int samplerate { get; set; }
-        int size { get; set; }
+        public string id_file { get; set; }
+        public string filename { get; set; }
+        public int bitrate { get; set; }
+        public int samplerate { get; set; }
+        public int size { get; set; }
     }
     class file_available
     {
-        string name { get; set; }
-        int bitrate { get; set; }
-        int samplerate { get; set; }
-        int size { get; set; }
-        int block_avail { get; set; }
-        private bool full { get; set; }
+        public int id_file;
+        public string nama { get; set; }
+        public int bitrate { get; set; }
+        public int samplerate { get; set; }
+        public int size { get; set; }
+        public int block_avail { get; set; }
+        public bool full { get; set; }
+        public bool isFull()
+        {
+            // if framelength*block available + header == size this is true
+            return true;
+        }
+        public int frameSize()
+        {
+            // the formula is 144 * bitrate * 1000 / samplingRate + paddingSize;
+            return 0;
+        }
     }
     class config
     {
