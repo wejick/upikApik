8952b1f4c9031505adafb5bbe7ef8eb231dbe7d6
Gian Giovani <wejick@gmail.com>
2013-08-07 23:18:47 +0700

    FIX file_host_rel id_file to integer

--- upikapik/upikapik/db.cs
+++ upikapik/upikapik/db.cs
@@ -1,24 +1,20 @@
-﻿/**
- * < Classes for database >
- * 
- * < -- >
- * < -- >
- * 
- **/
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace upikapik
 {
     class file_host_rel
     {
-        public string id_file { get; set; }
+        public int id_file { get; set; }
         public string ip { get; set; }
         public int block_avail { get; set; }
     }
     class file_list
     {
-        public string id_file { get; set; }
-        public string filename { get; set; }
+        public int id_file { get; set; }
+        public string nama { get; set; }
         public int bitrate { get; set; }
         public int samplerate { get; set; }
         public int size { get; set; }
