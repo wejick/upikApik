74f8e137151d69c4bcd1004a5087ce30f3821940
Gian Giovani <wejick@gmail.com>
2013-08-09 00:00:31 +0700

    FIX RedToHub class implementation
    Work with current Hub, (still need to be tested)

--- upikapik/upikapik/RedToHub.cs
+++ upikapik/upikapik/RedToHub.cs
@@ -1,62 +1,128 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 using Db4objects.Db4o;
 using Db4objects.Db4o.Linq;
-using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 namespace upikapik
 {
     class RedToHub
     {
         IObjectContainer db;
-        private string trackerIP {get; set;}
-        public RedToHub(string dbName)
+        private string trackerIP { get; set; }
+        private int port { get; set; }
+        private IPEndPoint server;
+        private string comToHub;
+        private string response;
+        private string[] parsedCommand;
+
+        public RedToHub(string dbName, string trackerIP, int port)
         {
             db = Db4oEmbedded.OpenFile(dbName);
+            this.trackerIP = trackerIP;
+            this.port = port;
+            server = new IPEndPoint(IPAddress.Parse(trackerIP), port);
         }
-        // get complete file list available at hub
-        public void getFileList()
+
+        // this is where the socket client and thread live :-D
+        public void connect(string command)
         {
-            string fileList = "[{\"id_file\":0,\"nama\":\"merindu.mp3\",\"bitrate\":\"128\",\"samplerate\":\"444000\",\"size\":6000},{\"id_file\":1,\"nama\":\"Menunggu.mp3\",\"bitrate\":\"128\",\"samplerate\":\"444000\",\"size\":6000},{\"id_file\":2,\"nama\":\"Menanti.mp3\",\"bitrate\":\"128\",\"samplerate\":\"444000\",\"size\":6000}]";
-            dynamic objects = JsonConvert.DeserializeObject(fileList);
-            foreach (var item in objects)
+            ThreadPool.QueueUserWorkItem(new WaitCallback(connectionHandler));
+            this.comToHub = command;
+            parsedCommand = comToHub.Split(';');
+        }
+        private void connectionHandler(object pStateObj)
+        {
+            //TcpClient red = new TcpClient(server);
+            TcpClient red = new TcpClient();
+            red.Connect(server);
+            // create buffer and set encoding to UTF8
+            //byte[] buffer = System.Text.Encoding.UTF8.GetBytes(comToHub);
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(comToHub);
+            // prepare stream to write or read
+            NetworkStream stream = red.GetStream();
+            // send command to HUB
+            if (parsedCommand[0] == "FL")
+            {
+                buffer = System.Text.Encoding.UTF8.GetBytes(parsedCommand[0]);
+            }
+            if (parsedCommand[0] == "FD")
+            {
+                buffer = System.Text.Encoding.UTF8.GetBytes(parsedCommand[0]+";;"+parsedCommand[1]);
+            }
+            if (parsedCommand[0] == "UP")
+            {
+                // command;;id_file;;block_avail
+                buffer = System.Text.Encoding.UTF8.GetBytes(parsedCommand[0] + ";;" + parsedCommand[1] + ";;" + parsedCommand[2]);
+            }
+            if (parsedCommand[0] == "NA")
+            {
+                buffer = System.Text.Encoding.UTF8.GetBytes(parsedCommand[0]);
+            }
+            stream.Write(buffer, 0, buffer.Length);
+
+            // clear buffer
+            buffer = new Byte[1024];
+            int byteCnt = stream.Read(buffer, 0, buffer.Length);
+            if(parsedCommand[0] == "FL"||parsedCommand[0] == "FD")
+                response = System.Text.Encoding.UTF8.GetString(buffer);
+            if (parsedCommand[0] == "FL")
+            {
+                getFileList(response);
+            }
+            if (parsedCommand[0] == "FD")
+            {
+                getFileDetail(response,Convert.ToInt16(parsedCommand[1]));
+            }
+            stream.Close();
+        }
+        public void printResponse()
+        {
+            Console.WriteLine("Isi response : {0}", response);
+        }
+        private void getFileList(string response)
+        {
+            List<file_list> list = JsonConvert.DeserializeObject<List<file_list>>(response);
+            dynamic all = from file_list f in db select f;
+            // delete all
+            foreach (var item in all)
+            {
+                db.Delete(item);
+            }
+            // write all
+            foreach (var item in list)
             {
                 db.Store(item);
             }
+            Console.WriteLine(response);
         }
-        // get file detail including available peer
-        public void getFd(string fileId)
+        private void getFileDetail(string response, int id)
         {
-            string fileDetail = "[{\"id_file\":1,\"ip\":\"192.168.0.1\",\"block_avail\":20},{\"id_file\":1,\"ip\":\"192.168.0.3\",\"block_avail\":20},{\"id_file\":1,\"ip\":\"192.168.0.23\",\"block_avail\":20}]";
-            List<file_host_rel> update = new List<file_host_rel>();
-            dynamic objects = JsonConvert.DeserializeObject(fileDetail);
-            dynamic obj;
-            // if exist update, else insert
-            // not yet tested
-            foreach (var item in objects)
+            string fileDetail = response;
+            List<file_host_rel> update = JsonConvert.DeserializeObject<List<file_host_rel>>(response);
+            dynamic all_ok = from file_host_rel f in db where f.id_file.Equals(id) select f;
+            // delete all apropriate
+            foreach (var item in all_ok)
             {
-                int id = item.id;
-                string ip = item.ip;
-                obj = from file_host_rel f in db
-                      where f.id_file.Equals(id) && f.ip.Equals(ip)
-                      select f;
-                if (obj != null)
-                {
-                    obj.block_avail = item.block_avail;
-                    db.Store(obj);
-                } else
-                    db.Store(item);
+                db.Delete(item);
             }
-
-        }
-        // update information of your file at hub
-        public void update(int fileId, int block_avail)
-        {
-            string command;
-            dynamic fileInfo = from file_available f in db
-                               where f.id_file == fileId
-                               select f;
-            command = "UP;;"+fileInfo.id_file+";;ipku;;"+fileInfo.block_avail;
+            // insert all from response
+            foreach (var item in update)
+            {
+                Console.WriteLine(item.ip);
+                db.Store(item);
+            }
+            all_ok = from file_host_rel f in db where f.id_file.Equals(id) select f;
+            foreach (var item in all_ok)
+            {
+                Console.WriteLine(item.ip);
+            }
+            Console.WriteLine(response);
         }
     }
 }
